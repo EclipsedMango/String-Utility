d544dc956b8bbe6694403129717ffa622ef08904
Michael <140897468+EclipsedMango@users.noreply.github.com>
2025-02-13 14:12:31 +1100

    added FindCharacter, Replace and ReadFromConsole functions.

--- String.cpp
+++ String.cpp
@@ -4,11 +4,14 @@
 #include <cctype>
 
 // Constructors
+
+// Create a string with a size and null termination character.
 String::String() {
 	string = nullptr;
 	size = 0;
 }
 
+// Copy the string to _other.
 String::String(const String& _other) {
 	size = _other.size;
 	string = new char[size + 1];
@@ -16,6 +19,7 @@ String::String(const String& _other) {
 	strcpy(string, _other.string);
 }
 
+// Copy the characters to _str.
 String::String(const char* _str) {
 	size = strlen(_str);
 	string = new char[size + 1];
@@ -24,6 +28,25 @@ String::String(const char* _str) {
 }
 
 // Utility Functions
+
+// Find the length then return that length.
+size_t String::Length() const {
+	return strlen(string);
+}
+
+// Check the size of the string, add to it if needed and add the other string.
+String& String::Append(const String& _str) {
+	if (sizeof(string) > size + _str.size + 1) {
+		return *this;
+	}
+
+	strcat(string, _str.string);
+	size += _str.size;
+
+	return *this;
+}
+
+// Iterates through string replacing characters with lower case characters.
 String& String::ToLower() {
 	for (size_t i = 0; i < size; ++i) {
 		string[i] = tolower(string[i]);
@@ -32,6 +55,7 @@ String& String::ToLower() {
 	return *this;
 }
 
+// Iterates through string replacing characters with upper case characters.
 String& String::ToUpper() {
 	for (size_t i = 0; i < size; ++i) {
 		string[i] = toupper(string[i]);
@@ -40,6 +64,44 @@ String& String::ToUpper() {
 	return *this;
 }
 
+// Iterate through string and check it the char matches anything in string.
+int String::FindCharacter(const char _chr) {
+	for (size_t i = 0; i < size; ++i) {
+		if (string[i] == _chr) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+
+int String::Replace(const char _find, const char _replace) {
+	for (size_t i = 0; i < size; ++i) {
+		if (string[i] == _find) {
+			string[i] = _replace;
+		}
+	}
+
+	return -1;
+}
+
+String& String::ReadFromConsole() {
+	std::string str;
+	std::cin >> str;
+
+	for (size_t i = 0; i < str.size(); ++i) {
+		string[i] = str[i];
+
+		if (string[i] == '\0') {
+			return *this;
+		}
+	}
+
+	return *this;
+}
+
+// Iterates through characters and writes them to console until null termination character.
 String& String::WriteToConsole() {
 	for (size_t i = 0; i < strlen(string); ++i) {
 		std::cout << string[i];
--- StringUtility.cpp
+++ StringUtility.cpp
@@ -7,8 +7,32 @@
 using namespace std;
 
 int main() {
-	String blah = "BLah";
-	blah.WriteToConsole();
+	String text = "Hallo World!";
+	String originalText = text;
+
+	std::cout << "String length " << text.Length() << "\n";
+
+	text.WriteToConsole();
+	std::cout << "\n";
+
+	text.ToLower();
+	text.WriteToConsole();
+	std::cout << "\n";
+
+	text.ToUpper();
+	text.WriteToConsole();
+	std::cout << "\n";
+
+	originalText.Append(" I Appended this!");
+	originalText.WriteToConsole();
+	std::cout << "\n";
+
+	std::cout << "Character W is at index: " << originalText.FindCharacter('W') << "\n";
+	std::cout << "Character K is at index: " << originalText.FindCharacter('K') << "\n";
+
+	text.ToLower();
+	text.Replace('l', 'j');
+	text.WriteToConsole();
 
 	return 0;
 }
